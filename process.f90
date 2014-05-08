e2503afebd4b9e6ed2c515e05eda837201152976
Knight, Peter J <peter.knight@ccfe.ac.uk>
2014-05-08 11:51:04 +0100

    08/05/2014 (r267) Updated TF coil picture and description in User Guide

--- process.f90
+++ process.f90
@@ -358,7 +358,7 @@ subroutine inform(progid)
 
   character(len=10) :: progname
   character(len=*), parameter :: progver = &  !  Beware: keep exactly same format...
-       '266    Date  :: 2014-05-08'
+       '267    Date  :: 2014-05-08'
   character(len=72), dimension(10) :: id
 
   !  External routines
@@ -1529,3 +1529,4 @@ end subroutine output
 ! GIT 264: Fixed progver format problem
 ! GIT 265: Removed wpvf usage
 ! GIT 266: New draft of stress model; replaced itfmod and stress_model with tfc_model
+! GIT 267: Updated TF coil picture and description in User Guide
