135f2986856df8c83832db3792a64980b6e00164
James Edwards <James.Edwards@ccfe.ac.uk>
2014-07-15 16:16:58 +0100

    Tool to extract variables and their descriptions

---
+++ web/variable_scraper.py
@@ -0,0 +1,36 @@
+import os
+
+GLOBAL_VARS_FILE = os.path.join(os.getcwd(), os.pardir, "global_variables.f90")
+
+def extract_var_desc(line):
+	#print("PROCESSING:", line)
+	chunks = line.split(":")
+	var_name = chunks[0].split()[1]
+	description = chunks[1]
+	return var_name, description
+
+def get_lines(filename):
+	with open(filename) as fh:
+		lines = fh.readlines()
+		linegen = (line.strip() for line in lines)
+	return linegen
+
+def extract_all_vars(filename):
+	lines = None
+	try:
+		lines = get_lines(filename)
+	except IOError:
+		print("Le fail...")
+		
+	variables = {}
+	for line in lines:
+		if line.startswith("!+ad_vars"):
+			try:
+				variable_name, description = extract_var_desc(line)
+				variables[variable_name] = description
+			except Exception as e:
+				print(e, line)
+	return variables
+
+if __name__ == "__main__":
+	extract_all_vars(GLOBAL_VARS_FILE)
