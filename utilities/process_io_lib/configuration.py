40ef49489470b06c19fe4056b42fa3e625cfdf31
James Edwards <James.Edwards@ccfe.ac.uk>
2014-11-23 15:47:43 +0000

    Remove unused default keyword. Reduce verbosity on using default. Still raises KeyError when nothing found and no default available.

--- utilities/process_io_lib/configuration.py
+++ utilities/process_io_lib/configuration.py
@@ -81,7 +81,7 @@ class Config(object):
         else:
             return objekt
     
-    def _search_config_for(self, config, default=None, *keys):
+    def _search_config_for(self, config, *keys):
         """Recursively search config (a dict) for keys."""
         try:
             if isinstance(keys[0], str):
@@ -90,7 +90,6 @@ class Config(object):
                 search_key = keys[0]
             value = config[search_key]
         except IndexError:
-            print(keys)
             raise
         except KeyError:
             raise
@@ -119,12 +118,12 @@ class Config(object):
         try:
             return self._search_config_for(self.config_data, *config_keys)
         except KeyError:
-            api_logger.exception("Cannot find value for {} in "
-                                 "configuration".format(config_keys))
             if default:
+                api_logger.info("Using default for {}".format(config_keys))
                 return default
             else:
-                raise
-        except:
+                api_logger.exception("Cannot find value or default for {} in "
+                                     "configuration".format(config_keys))
+        except (IndexError, TypeError):
             raise
         
