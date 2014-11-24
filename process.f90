cf46465e19027a21c161a636bb8c14c17ebc6b68
Knight, Peter J <peter.knight@ccfe.ac.uk>
2014-11-24 12:06:43 +0000

    24/11/2014: (r371) Minor changes to User Guide

--- process.f90
+++ process.f90
@@ -274,7 +274,7 @@ subroutine inform(progid)
   character(len=*), parameter :: tempfile = 'SCRATCHFILE.DAT'
   character(len=10) :: progname
   character(len=*), parameter :: progver = &  !  Beware: keep exactly same format...
-       '370    Release Date :: 2014-11-24'
+       '371    Release Date :: 2014-11-24'
   character(len=72), dimension(10) :: id
   integer :: unit
   logical :: unit_available
@@ -1862,3 +1862,4 @@ end subroutine output
 ! GIT 368: Minor changes to User Guide
 ! GIT 369: Updated in_dat.py
 ! GIT 370: Corrected dcond usage for resistive PF coils
+! GIT 371: Minor changes to User Guide
