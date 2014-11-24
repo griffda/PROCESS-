0b477d774890a96d3a92c3ef21eacb9d11545ecc
Knight, Peter J <peter.knight@ccfe.ac.uk>
2014-11-24 09:22:19 +0000

    24/11/2014: (r369) Updated in_dat.py

--- process.f90
+++ process.f90
@@ -274,7 +274,7 @@ subroutine inform(progid)
   character(len=*), parameter :: tempfile = 'SCRATCHFILE.DAT'
   character(len=10) :: progname
   character(len=*), parameter :: progver = &  !  Beware: keep exactly same format...
-       '368    Release Date :: 2014-11-17'
+       '369    Release Date :: 2014-11-24'
   character(len=72), dimension(10) :: id
   integer :: unit
   logical :: unit_available
@@ -1860,3 +1860,4 @@ end subroutine output
 !          power balance constraints
 ! GIT 367: Added output_costs switch to turn on/off costing information to file
 ! GIT 368: Minor changes to User Guide
+! GIT 369: Updated in_dat.py
