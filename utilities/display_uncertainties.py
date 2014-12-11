9e517379f0fff00b3a2b619c63417ca0f0be290c
Hanni <Hanni.Lux@ccfe.ac.uk>
2014-12-11 15:23:19 +0000

    updated arguments in display_uncertainties.py, specific variable treatment still needs to be programmed

--- utilities/display_uncertainties.py
+++ utilities/display_uncertainties.py
@@ -19,6 +19,46 @@ from numpy import loadtxt
 from matplotlib.ticker import NullFormatter
 from pylab import figure, axes, show, savefig
 
+
+def fig_2dscatter_and_hist(x,y):
+
+    """ function to create a 2d scatter plot with histograms"""
+    nullfmt   = NullFormatter()
+ 
+    figsize=(8,8)
+    left, width = 0.14, 0.61
+    bottom, height = 0.14, 0.61
+    bottom_h = left_h = left+width
+
+    # start with a rectangular Figure
+    figure(figsize=figsize)
+
+    axScatter = axes([left, bottom, width, height])
+    axHistx = axes([left, bottom_h, width, 0.2])
+    axHisty = axes( [left_h, bottom, 0.2, height])
+
+    # no labels
+    axHistx.xaxis.set_major_formatter(nullfmt)
+    axHistx.yaxis.set_major_formatter(nullfmt)
+    axHisty.xaxis.set_major_formatter(nullfmt)
+    axHisty.yaxis.set_major_formatter(nullfmt)
+
+    # the scatter plot:
+    axScatter.scatter(x, y,edgecolors='None')
+    axScatter.set_xlabel(labels[i])
+    axScatter.set_ylabel(labels[i+1])
+
+    bins = 10
+    axHistx.hist(x, bins=bins)
+    axHisty.hist(y, bins=bins, orientation='horizontal')
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
 
 ###########################################################
@@ -37,12 +77,19 @@ if __name__ == '__main__':
                         help="file format default =\
  .pdf")
 
+
+    PARSER.add_argument("variables", metavar='v', type=str,
+                        default='all', nargs='*',
+                        help="list of variables to be plotted; \
+default = all")
+
+
+
     ARGS = PARSER.parse_args()
 
 
 ############################################################
 #main program
-
 
     filename = ARGS.filename
 
@@ -53,40 +100,15 @@ if __name__ == '__main__':
 
     data = loadtxt('UNCERTAINTIES.DAT',skiprows=1)
 
-    for i in range(len(data[0])-1):
-        x = data[:,i]
-        y = data[:,i+1]
+    if ARGS.variables == 'all':
+        for i in range(len(data[0])-1):
+            x = data[:,i]
+            y = data[:,i+1]
 
-        nullfmt   = NullFormatter()
-
-        figsize=(8,8)
-        left, width = 0.14, 0.61
-        bottom, height = 0.14, 0.61
-        bottom_h = left_h = left+width
-
-        # start with a rectangular Figure
-        figure(figsize=figsize)
-
-        axScatter = axes([left, bottom, width, height])
-        axHistx = axes([left, bottom_h, width, 0.2])
-        axHisty = axes( [left_h, bottom, 0.2, height])
-
-        # no labels
-        axHistx.xaxis.set_major_formatter(nullfmt)
-        axHistx.yaxis.set_major_formatter(nullfmt)
-        axHisty.xaxis.set_major_formatter(nullfmt)
-        axHisty.yaxis.set_major_formatter(nullfmt)
-
-        # the scatter plot:
-        axScatter.scatter(x, y,edgecolors='None')
-        axScatter.set_xlabel(labels[i])
-        axScatter.set_ylabel(labels[i+1])
-
-        bins = 10
-        axHistx.hist(x, bins=bins)
-        axHisty.hist(y, bins=bins, orientation='horizontal')
-        savefig('Uncertainties_'+labels[i]+'_'+labels[i+1]+ARGS.end)
-
+            fig_2dscatter_and_hist(x,y)
+            savefig('Uncertainties_'+labels[i]+'_'+labels[i+1]+ARGS.end)
+    else: 
+        pass#TODO program this option
     
     
 show()
