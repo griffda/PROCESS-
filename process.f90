174ba1239bb420d76e0a348ca4fedcec05b023d2
Hanni <Hanni.Lux@ccfe.ac.uk>
2014-12-19 16:18:03 +0000

    lots of modified files after merge with James E.s branch, not sure why they were modified

--- process.f90
+++ process.f90
@@ -274,7 +274,7 @@ subroutine inform(progid)
   character(len=*), parameter :: tempfile = 'SCRATCHFILE.DAT'
   character(len=10) :: progname
   character(len=*), parameter :: progver = &  !  Beware: keep exactly same format...
-       '368    Release Date :: 2014-11-17'
+       '371    Release Date :: 2014-11-24'
   character(len=72), dimension(10) :: id
   integer :: unit
   logical :: unit_available
@@ -1860,3 +1860,6 @@ end subroutine output
 !          power balance constraints
 ! GIT 367: Added output_costs switch to turn on/off costing information to file
 ! GIT 368: Minor changes to User Guide
+! GIT 369: Updated in_dat.py
+! GIT 370: Corrected dcond usage for resistive PF coils
+! GIT 371: Minor changes to User Guide
