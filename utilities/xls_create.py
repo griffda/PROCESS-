67d02a256de4e8b5bb8cd80d83ecee16e37501d0
Michael Kovari <michael.kovari@ccfe.ac.uk>
2017-07-31 15:39:14 +0100

    xls script improved

--- utilities/xls_create.py
+++ utilities/xls_create.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 
 """
-
-  Append summary data to a spreadsheet data_sumamry.xlsx or name specified.
+  Append summary data to a spreadsheet data_summary.xlsx or name specified.
 
   Michael Kovari
 
     + 31/07/2017: Initial version created
-
 """
 
 import os
 import argparse
 import process_io_lib.mfile as mf
 from process_io_lib.mfile import make_plot_dat
-#from process_io_lib.process_dicts import PARAMETER_DEFAULTS
 from openpyxl import Workbook, styles, load_workbook
 from openpyxl.styles import  Font, Border, Side
 
@@ -48,25 +45,26 @@ def write_column_mplot_dat(spreadsheet, custom_keys, mfile_data):
             val_keys.append(key)
             var_names = var_names + [key]
 
-    ws.append(var_descriptions)
-    ws.append(var_names)    
+    # Print the descriptions only once
+    if ws.max_row == 1:
+        ws.append(var_descriptions)
+
+    ws.append(var_names)
 
     # Write rows of values. One row for each scan.
     for num in range(num_scans):
          new_row = ['']
-         values = ""
+         value = ""
          for vkey in val_keys:
              if vkey in header_variables:
                  # These are found only once at the top of the MFILE.
-                 values = mfile_data.data[vkey].get_scan(-1)
+                 value = mfile_data.data[vkey].get_scan(-1)
              else:
                 # These are present in the MFILE for each scan point
-                values = mfile_data.data[vkey].get_scan(num+1)
-             new_row = new_row + [values]
+                value = mfile_data.data[vkey].get_scan(num+1)
+             new_row = new_row + [value]
 
          ws.append(new_row)
-         print('new_row')
-         print(new_row)
 
     # Save the spreadsheet
     wb.save(spreadsheet)
@@ -77,22 +75,19 @@ def write_column_mplot_dat(spreadsheet, custom_keys, mfile_data):
 if __name__ == "__main__":
 
     # Setup command line arguments
-    parser = argparse.ArgumentParser(description='Add data to spreadsheet')
+    parser = argparse.ArgumentParser(description='Append summary data to a spreadsheet data_summary.xlsx or name specified.')
 
     parser.add_argument('-p', metavar='p', type=str, nargs='+',
                         help='add new variables to the output')
 
-    parser.add_argument('-f', metavar='f', type=str,
-                        help='File to read as MFILE.DAT')
+    parser.add_argument('-f', metavar='f', type=str, help='File to read as MFILE.DAT')
 
     parser.add_argument('-x', metavar='x', type=str,
-                        help='spreadsheet (xlsx) file to append to')
+                        help='spreadsheet (.xlsx) file to append to')
 
-    parser.add_argument("--defaults", help="run with default params",
-                        action="store_true")
+    parser.add_argument("--defaults", help="run with default params", action="store_true")
 
-    parser.add_argument("--reset-config", help="Reset xls.conf",
-                        action="store_true")
+    parser.add_argument("--reset-config", help="Reset xls.conf", action="store_true")
 
     args = parser.parse_args()
 
@@ -108,11 +103,10 @@ if __name__ == "__main__":
 
     # If user has specified a spreadsheet file that isn't data_summary.xlsx
     if args.x:
-        print('x', x)
         spreadsheet = args.x
     else:
         spreadsheet = 'data_summary.xlsx'
-    print(args)
+
     print('spreadsheet name', spreadsheet)
 
     # Get files in current directory to check for the config file.
@@ -138,7 +132,7 @@ if __name__ == "__main__":
         conf_file.close()
 
     INPUT_CONFIG = mf.read_mplot_conf("xls.conf")
-    print('INPUT_CONFIG')
+    print('Variables to be output:')
     print(INPUT_CONFIG)
 
     if args.defaults:
