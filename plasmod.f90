adbe6f1e88969a78037ca8d66db2ef51aa869a82
Lux, Hanni <hanni.lux@ukaea.uk>
2018-03-14 16:02:54 +0000

    HL 14/03/2018: Corrected changes by EF after discussion via e-mail.

--- plasmod.f90
+++ plasmod.f90
@@ -100,10 +100,10 @@ contains
     ! These values should only be set, if the respective constraints
     ! are being used. They should be set to large values otherwise.
     ! pseprmax and psepbqarmax cannot be used at the same time!
-    if (any(ixc == 97)) then
+    if (any(icc == 56)) then
        comp%psep_r      = pseprmax*fpsepr !Psep/R max value
        comp%psepb_q95AR = 1.0e3 !large number to have no effect
-    else if (any(ixc == 117)) then
+    else if (any(icc == 68)) then
        comp%psep_r      = 1.0e3 !large number to have no effect
        comp%psepb_q95AR = psepbqarmax*fpsepbqar !Psep B/qaR max value times the iteration variable
        
