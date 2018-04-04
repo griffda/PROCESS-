522fb994f9ca6b20f260846b478c06429591ed87
Katy Ellis <Katy.Ellis@ukaea.uk>
2018-04-04 09:36:21 +0100

    updates to radial plotting script

--- utilities/compare_radials.py
+++ utilities/compare_radials.py
@@ -22,28 +22,27 @@ from process_io_lib.profile_funcs import nresidual, tresidual, nprofile, \
 from pylab import figure, xlabel, ylabel, axvline, axhline, plot, show
 
 def plotProfile(i,p, count):
-    print('size of RHO[0] = '+ repr(len(RHO[0])))
-    print('size of DATA_PROFILES[0][i] = '+ repr(len(DATA_PROFILES[0][i])))
-    print(DATA_PROFILES[0][i])
+    #print('size of RHO[0] = '+ repr(len(RHO[0])))
+    #print('size of DATA_PROFILES[0][i] = '+ repr(len(DATA_PROFILES[0][i])))
+    #print(DATA_PROFILES[0][i])
 
     #for i in range(0,len(ASSIGN_PAGE)):
     #    if(ASSIGN_PAGE[i]==p):
-            if(count==0): figure=221
-            if(count==1): figure=222
-            if(count==2): figure=223
-            if(count==3): figure=224
-            page[p].add_subplot(figure)
+    if(count==0): figure=221
+    if(count==1): figure=222
+    if(count==2): figure=223
+    if(count==3): figure=224
+    page[p].add_subplot(figure)
     
-            plt.scatter(RHO[0], DATA_PROFILES[0][i])
-            plt.scatter(RHO[1], DATA_PROFILES[1][i])
-
-            plt.xlabel('r/a')
-            plt.ylabel(AXIS_TITLES[i])
-            plt.title(PLOT_TITLES[i])
-            count=count+1
-    #plt.plot(RHO[0],DATA_PROFILES[0][i], label="File0")
-    #plt.plot(RHO[1],DATA_PROFILES[1][i], label="File1")
-    #plt.legend()
+    plt.scatter(DATA_PROFILES[0][0], DATA_PROFILES[0][i])
+    plt.scatter(DATA_PROFILES[1][0], DATA_PROFILES[1][i])
+    
+    plt.xlabel(AXIS_TITLES[0])
+    plt.ylabel(AXIS_TITLES[i])
+    plt.title(PLOT_TITLES[i])
+    plt.plot(DATA_PROFILES[0][0],DATA_PROFILES[0][i], label="File0")
+    plt.plot(DATA_PROFILES[1][0],DATA_PROFILES[1][i], label="File1")
+    plt.legend()
 
 if __name__ == '__main__':
         
@@ -65,13 +64,14 @@ if __name__ == '__main__':
     
     ARGS = PARSER.parse_args()
 
-    LIST_PROFILES = ['DEN', 'TE', 'TI', 'DEUT', 'TRIT', 'JBS', 'JCD', 'JTOT', 'IPOL', 'Q', 'VOL', 'DVOL', 'COND']
+    LIST_PROFILES = ['RHO', 'DEN', 'TE', 'TI', 'DEUT', 'TRIT', 'JBS', 'JCD', 'JTOT', 'IPOL', 'Q', 'VOL', 'DVOL', 'COND']
     list_len = len(LIST_PROFILES)
-    print('number of profiles to be plotted = ' + repr(list_len))
+    print('number of profiles to be plotted = ' + repr(list_len-1))
     NUM_PAGES = 4
-    ASSIGN_PAGE = [1, 2, 2, 1, 1, 3, 3, 3, 3, 1, 4, 4, 2] #Which page for each plot (4 plots per page)
+    #ASSIGN_PAGE = [1, 2, 2, 1, 1, 3, 3, 3, 3, 1, 4, 4, 2] #Which page for each plot (4 plots per page)
 
-    PLOT_TITLES = ['Electron density']
+    PLOT_TITLES = [' ' ]
+    PLOT_TITLES.append('Electron density')
     PLOT_TITLES.append('Electron temperature')
     PLOT_TITLES.append('Ion temperature')
     PLOT_TITLES.append('Deuterium density')
@@ -85,7 +85,8 @@ if __name__ == '__main__':
     PLOT_TITLES.append('dVolume/dr')
     PLOT_TITLES.append('Plasma conductivity')
 
-    AXIS_TITLES = ['ne / 1e19 m-3']
+    AXIS_TITLES = ['r/a']
+    AXIS_TITLES.append('ne / 1e19 m-3')
     AXIS_TITLES.append('te / keV')
     AXIS_TITLES.append('ti / keV')
     AXIS_TITLES.append('deut / 1e19 m-3')
@@ -99,23 +100,22 @@ if __name__ == '__main__':
     AXIS_TITLES.append('dVol/dr / m^2')
     AXIS_TITLES.append('Cond / MA/(V.m)')
 
-    PAGE_TITLES = ['Radial profiles (page 1) - densities']
+    PAGE_TITLES = ['Radial profiles (page 1)']
     PAGE_TITLES.append('Radial profiles (page 2)')
-    PAGE_TITLES.append('Radial profiles (page 3) - currents')
+    PAGE_TITLES.append('Radial profiles (page 3)')
     PAGE_TITLES.append('Radial profiles (page 4)')
     
     fileArray = [ARGS.filename0, ARGS.filename1]
     n = len(fileArray)
     print('number of files is: ' + repr(n))
     DATA = [None] * n
-    RHO = [None] * n
-
-    DATA_PROFILES = [LIST_PROFILES] * n
+    #DATA_PROFILES = [LIST_PROFILES] * n
+    DATA_PROFILES = [[0]*14 for i in range(n)]
     
     #create vectors to store the data
     useProfile = [None] * list_len
     for i in range(0,list_len):
-        useProfile[i] = True
+        useProfile[i] = False
 
     for f in range(0,n):  #loop over files
         print('filename' + repr(f) + ' = ' + repr(fileArray[f]))
@@ -133,60 +133,40 @@ if __name__ == '__main__':
         if DATA[f].size == 0:
             print('Error: There is no data in', fileArray[f])
             exit()
-            
-        try:
-            RHO[f] = DATA[f][0]
-            print('Inputted RHO')
-            #print(RHO[f])
-        except IndexError:
-            print('Error: The column for the normalised radius does not exist!\
-            Remember to start counting at 0!')
-            exit()
 
-        for i in range(1,list_len):
+        for i in range(0,list_len):
             try:
-                #print('f = '+repr(f)+' i = '+repr(i))
                 DATA_PROFILES[f][i] = DATA[f][i]
-                #print(DATA_PROFILES[f][i])
-                #print('Inputted ' + repr(LIST_PROFILES[i-1])) #prints entire array
+                useProfile[i] = True
+                print('f = ' + repr(f) + ' i = ' + repr(i))
             except IndexError:
-                print('Warning: The column for the '+repr(LIST_PROFILES[i-1])+' in file '+repr(f)+' does not exist!\
+                print('Warning: The column for the '+repr(LIST_PROFILES[i])+' in file '+repr(f)+' does not exist!\
                 Remember to start counting at 0!')
-                useProfile[i] = False
         print(useProfile)
-        #print(RHO[0])
-        #print(DATA_PROFILES[0][1])
-        #print(RHO[1])
-        #print(DATA_PROFILES[1][1])
 
-    ###################
-    #count1 = 0
+    #Make plots
+    i = 1 #i.e. don't plot rho against itself
     page = [None] * NUM_PAGES
     for p in range(0,NUM_PAGES):
         page[p] = plt.figure(figsize=(12, 9), dpi=80)
         plt.suptitle(PAGE_TITLES[p])
-            
-        for i in range(2,3):   #list_len
-            plotProfile(i, p)
-                #if(count1==1):
-                #     page[p].add_subplot(222)
-                #     plotProfile(i, p)
-
-    #plot_den = page1.add_subplot(221)
-
-    print(RHO[0])
-    print(DATA_PROFILES[0][1])
-    print(RHO[1])
-    print(DATA_PROFILES[1][1])
+        count = 0    
+        while i < list_len:
+            #print('i, p, count = ' + repr(i) + ', ' + repr(p) + ', ' + repr(count)) 
+            plotProfile(i, p, count)
+            i  = i + 1
+            count = count + 1   
+            if count == 4:
+                break
     
     #page1.savefig('../KatyTest/profile_plots.jpg')
 
     with bpdf.PdfPages("../KatyTest/radial_profiles.pdf") as pdf:
+        pdf.savefig(page[0])
         pdf.savefig(page[1])
-   #     pdf.savefig(page[2])
-   #     pdf.savefig(page[3])
-   #     pdf.savefig(page[4])
-    plt.show(page[1])
+        pdf.savefig(page[2])
+        pdf.savefig(page[3])
+    plt.show(page[0])
    # plt.show(page[2])
    # plt.show(page[3])
    # plt.show(page[4])
