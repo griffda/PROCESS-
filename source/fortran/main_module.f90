71dde27b6b509deaeb820cb5e8e37b71d9d0d709
James Morris <james.morris2@ukaea.uk>
2019-03-15 14:23:01 +0000

    Updated version number across files for tag 1.0.15

--- source/fortran/main_module.f90
+++ source/fortran/main_module.f90
@@ -68,7 +68,7 @@ subroutine inform(progid)
   character(len=10) :: progname
   character(len=98) :: executable
   character(len=*), parameter :: progver = &  !  Beware: keep exactly same format...
-       '1.0.14   Release Date :: 2018-11-09'
+       '1.0.15   Release Date :: 2019-03-15'
   character(len = 50) :: dt_time
   character(len=72), dimension(10) :: id
   ! integer :: unit
