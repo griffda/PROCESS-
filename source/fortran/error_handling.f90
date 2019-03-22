647404a7d48ecd6bf194c29d8157b4c3b3aa4471
James Morris <james.morris2@ukaea.uk>
2019-03-22 17:21:04 +0000

    See issue #760

--- source/fortran/error_handling.f90
+++ source/fortran/error_handling.f90
@@ -84,8 +84,8 @@ module error_handling
   !  int and float arrays may be useful to provide diagnostic information
 
   type :: error
-     integer           :: level    !  severity level
-     character(len=80) :: message  !  information string
+     integer            :: level    !  severity level
+     character(len=200) :: message  !  information string
      integer, dimension(8) :: idiags = INT_DEFAULT
      real(kind(1.0D0)), dimension(8) :: fdiags = FLT_DEFAULT
   end type error
