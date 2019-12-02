1fec78a65bb3423a811e424d8c0d80ce30a235e3
skahn <sebastien.kahn@ukaea.uk>
2019-12-02 17:39:43 +0000

    Issue-#936 : Removing constraint 10  - 1/R dependency constrataint hard-coded in the (sc)tfcoil.f90 modules  - The variable oacdcp (average TF current density calculated including the TF nose that carry no current...) should be no longer an optimisation variable  - The constraints 10 and the variable 12 is not yet physically removed, but the check function makes sure these are not used.

--- source/fortran/global_variables.f90
+++ source/fortran/global_variables.f90
@@ -2242,8 +2242,11 @@ module tfcoil_variables
   !! n_layer /20/ : Number of layers in TF coil (i_tf_turns_integer=1)
 
   real(kind(1.0D0)) :: oacdcp = 1.4D7
-  !! oacdcp /1.4e7/ : overall current density in TF coil inboard legs (A/m2)
-  !!                  (iteration variable 12)
+  !! oacdcp /1.4e7/ : overall current density in TF coil inboard legs midplane (A/m2)
+  !!                  Rem SK : Not used in tfcoil to set the current any more
+  !!                            -> SHOULD NOT BE USED AS ITERATION VARIABLE 12 ANY MORE
+  !!                            -> This variable is calculated
+
   real(kind(1.0D0)) :: poisson = 0.3D0
   !! poisson /0.3/ : Poisson's ratio for TF stress calculation
   !!                 (assumed constant over entire coil)
