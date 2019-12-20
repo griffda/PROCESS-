f79fdcb6b9c916bd7e744864653dc40ef50095be
Jonathan Maddock <jonathan.maddock@ukaea.uk>
2019-12-20 17:05:09 +0000

    Added approximate rule coverage calculator

--- utilities/process_io_lib/input_validator.py
+++ utilities/process_io_lib/input_validator.py
@@ -8,8 +8,10 @@ feedback. This validation is based on a set of rules, which are defined in
 input_validator_rules.
 """
 import inspect
+import re
 from process_io_lib import in_dat
 from process_io_lib import input_validator_rules
+from rootdir import ROOTDIR as FORTRAN_SOURCE_ROOTDIR
 
 INPUT_FILE_PATH = "IN.DAT"
 # Todo: make dynamic
@@ -56,6 +58,29 @@ class RuleList(object):
         # Now create an instance of each rule class in a list
         self.rules = [rule_class() for rule_class in rule_classes]
 
+    def report_coverage(self):
+        """Report roughly how many input data variables are covered by rules."""
+        var_names = []
+        # List of variable names that can be parsed in the input file
+        var_name_regex = r"\s*case\s*\(['|\"](\w+)"
+        # Pattern for finding var names in Fortran source
+        input_reader_filename = FORTRAN_SOURCE_ROOTDIR + "/input.f90"
+
+        # Create list of variables that can be parsed
+        with open(input_reader_filename, 'r') as input_reader:
+            for line in input_reader.readlines():
+                match = re.match(var_name_regex, line)
+                if match:
+                    var_names.append(match.group(1))
+
+        # Compare with rules
+        rules_total = len(self.rules)
+        var_names_total = len(var_names)
+        coverage = (rules_total / var_names_total) * 100
+        coverage = "{0:.2f}".format(coverage)
+        print(f"Rule coverage: {rules_total} variable rules / "
+            f"{var_names_total} possible input variables ({coverage}%)")
+
     def filter_rules(self, filter):
         """Use a filter string to select certain rules by tag.
         
@@ -97,6 +122,7 @@ class RuleList(object):
 def main():
     """Create the rule list and validate each rule against the input data."""
     rule_list = RuleList()
+    rule_list.report_coverage()
     rule_list.validate_data()
     rule_list.print_results()
 
