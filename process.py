8797fd291162263b65ae8a581334fb0b74218f8c
Jonathan Maddock <jonathan.maddock@ukaea.uk>
2020-02-13 09:45:52 +0000

    Add input file diff creator to process.py

--- process.py
+++ process.py
@@ -9,6 +9,9 @@ import os
 import sys
 import argparse
 from utilities.process_io_lib import input_validator
+from shutil import copy
+from pathlib import Path
+from difflib import unified_diff
 
 PROCESS_EXE_PATH = "./bin/process.exe"
 
@@ -16,12 +19,24 @@ class Process(object):
     """A Process workflow based on command line arguments."""
     def __init__(self):
         """Parse command line arguments and run accordingly."""
+        # File paths
+        self.run_dir = None
+        self.input = None
+        self.ref_input = None
+        
+        # Run actions
         self.parse_args()
         self.check_root_dir()
         if self.args.build:
             self.build_process()
             self.create_dicts()
-        
+
+        # Find the input file and look for changes to it
+        # TODO: input file needs to be mandatory; positional arg
+        self.set_input()
+        self.set_ref_input()
+        self.input_file_diff()
+
         self.validate_input()
         self.run_process()
         if self.args.util:
@@ -40,6 +55,10 @@ class Process(object):
             metavar="input_file_path",
             default="IN.DAT",
             help="The path to the input file that Process runs on")
+        parser.add_argument("--ref_input",
+            "-r",
+            metavar="reference_input",
+            help="Reference input file to record changes against")
         parser.add_argument("--build",
             "-b",
             action="store_true",
@@ -69,6 +88,103 @@ class Process(object):
         """Build Process"""
         subprocess.run(["cmake", "-H.", "-Bbuild"])
         subprocess.run(["cmake", "--build", "build"])
+
+    def set_input(self):
+        """Validate the input file path, then store it.
+        
+        Also set the run directory according to the input file path.
+        """
+        input_path = Path(self.args.input)
+        if input_path.exists():
+            self.input = input_path
+            # Set input as Path object
+            self.run_dir = Path(input_path.parent)
+            # Set run directory as dir that contains the input file
+        else:
+            raise FileNotFoundError("Input file not found on this path.")
+
+    def set_ref_input(self):
+        """Find an input file to use as a reference for changes.
+        
+        Find or create a reference input file to compare with the current 
+        input file, to allow the changes to be tracked.
+        """
+        # self.args.ref_input: reference input file path; can already be set as 
+        # command line arg
+        if self.args.ref_input:
+            # Ref input specified as command line arg. Check it exists
+            path = Path(self.args.ref_input)
+            if path.exists():
+                # Store path object
+                self.ref_input = path
+            else:
+                raise FileNotFoundError("Reference input file not found; "
+                    "check the path.")
+            
+            # Input file exists
+            if "REF_IN.DAT" in self.ref_input.name:
+                # Ref file specified; got a ref file
+                pass
+            elif "IN.DAT" in self.ref_input.name:
+                # Regular input file specified. Make a reference copy as 
+                # REF_IN.DAT and update self.ref_input
+                self.create_ref_input()
+            else:
+                # File isn't an IN.DAT or a REF_IN.DAT
+                raise ValueError("Reference input file should end in IN.DAT or "
+                    "REF_IN.DAT")
+        else:
+            # Ref input not specified: try to find one in the input file's dir
+            ref_inputs = list(self.run_dir.glob("*REF_IN.DAT"))
+            if len(ref_inputs) == 0:
+                # No ref input files: create one from input file
+                self.create_ref_input()
+            elif len(ref_inputs) == 1:
+                # One pre-existing ref input file: set as ref input
+                self.ref_input = ref_inputs[0]
+            else:
+                # More than one ref file: error
+                raise Exception("More than one REF_IN.DAT found in this dir. "
+                    "Specifiy which one to use with the \"-r\" option, or "
+                    "remove the other ones.")
+
+        # Now either have raised an exception or have a ref input file set,
+        # with suffix "REF_IN.DAT"
+
+    def create_ref_input(self):
+        """Create a reference input file from a pre-existing input file."""
+        # First, ensure that self.ref_input is set as an IN.DAT to copy
+        if not self.ref_input:
+            # No IN.DAT specified to use as ref: use input file instead
+            self.ref_input = self.input
+
+        # Copy the ref input and save it with the REF_IN.DAT extension
+        # Then set this as the new ref input
+        new_ref_input_name = self.ref_input.name.replace("IN.DAT", "REF_IN.DAT")
+        new_ref_input = Path(self.run_dir).joinpath(new_ref_input_name)
+        copy(self.ref_input, new_ref_input)
+        self.ref_input = new_ref_input
+
+    def input_file_diff(self):
+        """Perform a diff between the reference input and input files.
+        
+        This compares the REF_IN.DAT and IN.DAT files to highlight the 
+        modifications.
+        """
+        # Run a diff on the files
+        before_file = open(self.ref_input)
+        after_file = open(self.input)
+        before = before_file.readlines()
+        after = after_file.readlines()
+        before_file.close()
+        after_file.close()
+        diff = unified_diff(before, after, fromfile=self.ref_input.name, 
+            tofile=self.input.name)
+
+        # Write diff to output file
+        diff_path = self.run_dir.joinpath("input.diff")
+        with open(diff_path, "w") as diff_file:
+            diff_file.writelines(diff)
 
     def create_dicts(self):
         """Create Python dictionaries"""
