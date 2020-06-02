8b4eca17fd254bcfd17c59bcfcdaf62fefb4efa7
Jonathan Maddock <jonathan.maddock@ukaea.uk>
2020-06-02 09:08:51 +0100

    Remove Process program: now compile Fortran as library

--- source/fortran/process.f90
+++
@@ -1,8 +0,0 @@
-program process
-  ! Simplest possible program to call the Process subroutine
-  ! This makes Python wrapping much easier, as can now just wrap process_module
-  ! and call it from an equally simple Python script instead
-  use process_module, only: process_subroutine
-  
-  call process_subroutine
-end program process
