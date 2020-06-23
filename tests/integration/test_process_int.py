80a0571ea5993d4364038b9b3beeb81dc3fe5d7f
Jonathan Maddock <jonathan.maddock@ukaea.uk>
2020-06-23 14:05:21 +0100

    Improve exception handling in integration test

--- tests/integration/test_process_int.py
+++ tests/integration/test_process_int.py
@@ -9,10 +9,16 @@ INPUT_FILE = "tracking/baseline_2018/baseline_2018_IN.DAT"
 # Convert input file path to absolute and string
 input_file_path = str(Path(INPUT_FILE).resolve())
 
-def test_process_main(monkeypatch):
+def test_process_main():
     """Test the process.main() function, which performs a Process run."""
     # TODO Actually check that the run succeeds; will only be able to do this
     # when the interface moves a little deeper into the Fortran. Access to ifail
     # is what is wanted. Can't assert anything yet, so for now call into the 
     # Fortran and check that an exception isn't thrown on the way.
-    process.main(args=["-i", input_file_path])
+    try:
+        process.main(args=["-i", input_file_path])
+        result = True
+    except:
+        result = False
+
+    assert result == True
