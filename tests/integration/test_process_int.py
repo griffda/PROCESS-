5e82795ecb562b7e445379a6d93f50bf83c57270
Jonathan Maddock <jonathan.maddock@ukaea.uk>
2020-11-25 18:01:54 +0000

    SingleRun and VaryRun integration tests for CLI args

--- tests/integration/test_process_int.py
+++ tests/integration/test_process_int.py
@@ -1,24 +1,51 @@
-"""Integration tests for the process.py module."""
+"""Integration tests for the main.py module."""
 from process import main
 from process import fortran
 from pathlib import Path
+from shutil import copy
 
-# Real input file for testing
-# TODO Move to own fixture?
-INPUT_FILE = "tracking/baseline_2018/baseline_2018_IN.DAT"
-# Convert input file path to absolute and string
-input_file_path = str(Path(INPUT_FILE).resolve())
-
-def test_process_main():
-    """Test the main.main() function, which performs a Process run."""
+def test_single_run(tmp_path):
+    """Test a SingleRun Process run with CLI args."""
     # TODO Actually check that the run succeeds; will only be able to do this
     # when the interface moves a little deeper into the Fortran. Access to ifail
     # is what is wanted. Can't assert anything yet, so for now call into the 
     # Fortran and check that an exception isn't thrown on the way.
-    try:
-        main.main(args=["-i", input_file_path])
-        result = True
-    except:
-        result = False
+    
+    # Single run dir path
+    # Chosen because it's a baseline and runs quickly
+    # TODO This path is silly: find a better way of doing this
+    single_dir = Path(__file__).parent.parent.parent / "tracking/baseline_2018"
+    
+    # Copy files to temp dir so output files are discarded
+    for test_file in single_dir.glob("*"):
+        dst = tmp_path / test_file.name
+        copy(test_file, dst)
 
-    assert result == True
+    # IN.DAT path in temp dir
+    input_path = tmp_path / "baseline_2018_IN.DAT"
+    input_file = str(input_path.resolve())
+
+    # Run a SingleRun with an explicitly defined IN.DAT
+    main.main(args=["-i", input_file])
+
+def test_vary_run(tmp_path):
+    """Test a VaryRun with CLI args.
+
+    :param tmp_path: temporary dir path fixture
+    :type tmp_path: Path
+    """
+    # Vary run dir path
+    # Chosen because it's the only VaryRun in the test suite, and is fast
+    vary_dir = Path(__file__).parent.parent / "regression/scenarios/starfire"
+
+    # Copy all files to temp dir so output files are discarded
+    for test_file in vary_dir.glob("*"):
+        dst = tmp_path / test_file.name
+        copy(test_file, dst)
+
+    # run_process.conf path in temp dir
+    conf_path = tmp_path / "run_process.conf"
+    conf_file = str(conf_path.resolve())
+
+    # Run a VaryRun with a custom conf file name
+    main.main(args=["--varyiterparams", "--varyiterparamsconfig", conf_file])
