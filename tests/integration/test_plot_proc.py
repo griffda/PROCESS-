a290336a4f472a2896ae2e724bea3a9d8c00acde
Jonathan Maddock <jonathan.maddock@ukaea.uk>
2020-11-30 11:56:53 +0000

    Share integration temp data fixture

--- tests/integration/test_plot_proc.py
+++ tests/integration/test_plot_proc.py
@@ -3,25 +3,15 @@ import pytest
 from pathlib import Path
 from process.io import plot_proc
 
-@pytest.fixture
-def mfile():
-    """Fixture for an MFILE for plot_proc to run on.
-
-    :return: mfile path
-    :rtype: Path
-    """
-    data_path = Path(__file__).parent / "data"
-    mfile = data_path / "baseline_2018_MFILE.DAT"
-    return mfile
-    
 # TODO Test for mfile in cwd again?
 
-def test_input_file(mfile):
+def test_input_file(temp_data):
     """Run plot_proc on an input MFILE and check for an output.
 
-    :param mfile: mfile path
-    :type mfile: Path
+    :return: temporary path containing data files
+    :rtype: Path
     """
+    mfile = temp_data / "baseline_2018_MFILE.DAT"
     mfile_str = str(mfile)
     plot_proc.main(args=["-f", mfile_str])
     
