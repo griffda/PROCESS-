24a7a04d1929fb2e2d6f99d85486d05c2e4d0aa5
Timothy Nunn <timothy.nunn@ukaea.uk>
2021-09-13 15:30:04 +0100

    TEST: rm f90wrap

--- cmake/f2py.cmake
+++ cmake/f2py.cmake
@@ -3,6 +3,7 @@
 #   Date      :   last modified 2020-11-09
 #
 #   Run f2py on the given files list
+INCLUDE(${CMAKE_SOURCE_DIR}/cmake/process_preprocessing.cmake)
 
 MACRO(F2PY)
     EXECUTE_PROCESS (
@@ -11,11 +12,16 @@ MACRO(F2PY)
     )
     STRING(STRIP ${CMAKE_PYTHON_ABI_VERSION} CMAKE_PYTHON_ABI_VERSION)
 
-    SET(F2PY_TARGET ${CMAKE_BINARY_DIR}/_${FORTRAN_INTERFACE_NAME}${CMAKE_PYTHON_ABI_VERSION})
-    SET(F2PY_OUTPUT ${PYTHON_MODULE_DIR}/_${FORTRAN_INTERFACE_NAME}${CMAKE_PYTHON_ABI_VERSION})
+    FindPreprocessingVars()
+
+    SET(F2PY_TARGET ${CMAKE_BINARY_DIR}/process_${CMAKE_PYTHON_ABI_VERSION})
+    SET(F2PY_OUTPUT ${PYTHON_MODULE_DIR}/process_${CMAKE_PYTHON_ABI_VERSION})
+    LIST(TRANSFORM PROCESS_SRCS PREPEND ${PROCESS_SRC_DIR}/ OUTPUT_VARIABLE PROCESS_SRC_PATHS)
+
     SET(F2PY_NAME "f2py")
     MESSAGE(STATUS "[f2py]: ")
     MESSAGE(STATUS "\tTarget: ${F2PY_TARGET}")
+    MESSAGE(STATUS "\tf2py flags: ${COMPILE_DEFINITIONS} ")
     ADD_CUSTOM_TARGET(
         ${F2PY_NAME}
         DEPENDS ${F2PY_TARGET} ${F2PY_OUTPUT}
@@ -23,14 +29,14 @@ MACRO(F2PY)
     IF(NOT CMAKE_HOST_APPLE)
         ADD_CUSTOM_COMMAND(
             OUTPUT ${F2PY_TARGET} ${F2PY_OUTPUT}
-            COMMAND echo \"Running f2py:\"\; LDFLAGS=-Wl,-rpath=\\$$ORIGIN/lib ${F2PY_NAME}-${F90WRAP_NAME} -c -m _${FORTRAN_INTERFACE_NAME} -L../process/lib/ -l${PROJECT_NAME} ${F90WRAP_OUTPUT_FILES} --build-dir ${CMAKE_BINARY_DIR}
+            COMMAND echo \"Running f2py:\"\; LDFLAGS=-Wl,-rpath=\\$$ORIGIN/lib ${F2PY_NAME} -c -L../process/lib/ -l${PROJECT_NAME} ${PROCESS_SRC_PATHS} --build-dir ${CMAKE_BINARY_DIR} -DINSTALLDIR="${CMAKE_SOURCE_DIR}" -DCOMMSG="${COMMIT_MSG}" -Dtagno="${GIT_TAG}" -Dbranch_name="${GIT_BRANCH}" -Duntracked="${GIT_DIFF}" -I${CMAKE_SOURCE_DIR}
             COMMAND ${CMAKE_COMMAND} -E copy ${F2PY_TARGET} ${F2PY_OUTPUT}
         )
     ELSE()
         ADD_CUSTOM_COMMAND(
             OUTPUT ${F2PY_TARGET} ${F2PY_OUTPUT}
             COMMAND echo "Running f2py to produce target '${F2PY_TARGET}':"
-            COMMAND ${F2PY_NAME}-${F90WRAP_NAME} -c -m _${FORTRAN_INTERFACE_NAME} -L../process/lib -l${PROJECT_NAME} ${F90WRAP_OUTPUT_FILES} --build-dir ${CMAKE_BINARY_DIR}
+            COMMAND ${F2PY_NAME}-${F90WRAP_NAME} -c  -L../process/lib -l${PROJECT_NAME} ${PROCESS_SRCS} --build-dir ${CMAKE_BINARY_DIR}
             COMMAND ${CMAKE_COMMAND} -E copy ${F2PY_TARGET} ${F2PY_OUTPUT}
         )
     ENDIF()
