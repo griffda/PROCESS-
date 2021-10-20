417b4acfd46edbbac8817abba480cd67ddc920fb
Charles Swanson <i.am.cswan@gmail.com>
2021-10-20 11:40:08 -0400

    Oops, there was an error call in initial that can be bypassed under certain conditions now

--- source/fortran/initial.f90
+++ source/fortran/initial.f90
@@ -804,7 +804,8 @@ subroutine check
     !    Generate a lvl 3 error proposing not to use any stress constraints
     if (       ( .not. ( any(ixc == 16 ) .or. any(ixc == 29 ) .or. any(ixc == 42 ) ) ) & ! No bore,gapoh, ohcth iteration  
          .and. ( abs(bore + gapoh + ohcth + precomp) < epsilon(bore) )                 & ! bore + gapoh + ohcth = 0
-         .and. ( any(icc == 31) .or. any(icc == 32) ) ) then                                                     ! Stress constraint (31) is used 
+         .and. ( any(icc == 31) .or. any(icc == 32) )                                  & ! Stress constraints (31 or 32) is used 
+         .and. ( i_tf_plane_stress /= 2 ) ) then                                         ! TF stress model can't handle no bore                                           
 
         call report_error(246)
         stop 1
