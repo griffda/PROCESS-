5a0e0cfa73521841c540b9cbabfe44227d1fbaf7
Timothy Nunn <timothy.nunn@ukaea.uk>
2021-11-30 09:54:56 +0000

    Document git.py

--- tracking/git.py
+++ tracking/git.py
@@ -1,8 +1,20 @@
+"""Simple submodule to provide access to some git attributes about the current repository"""
+
 from pathlib import Path
 import subprocess
 
 
 def git_commit_message(directory=None) -> str:
+    """Get the commit message for `directory` or, if `directory` is not provided, the current working directory.
+
+    Will raise a subprocess.CalledProcessError if the directory checked is not a git repository.
+
+    :param directory: the directory to get the commit message at
+    :type directory: str | bytes | path-like
+
+    :return commit_message: the commit message for the git repository
+    :type commit_message: str
+    """
     if directory is None:
         directory = Path.cwd()
 
@@ -18,6 +30,16 @@ def git_commit_message(directory=None) -> str:
 
 
 def git_commit_hash(directory=None) -> str:
+    """Get the commit hash for `directory` or, if `directory` is not provided, the current working directory.
+
+    Will raise a subprocess.CalledProcessError if the directory checked is not a git repository.
+
+    :param directory: the directory to get the commit hash for
+    :type directory: str | bytes | path-like
+
+    :return commit_hash: the commit hash for the git repository
+    :type commit_hash: str
+    """
     if directory is None:
         directory = Path.cwd()
 
