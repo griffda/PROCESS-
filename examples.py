c763130e1a54c5cd091cc79248d8cbf0ab94e2c1
-l <jonathan.maddock@ukaea.uk>
2021-12-01 11:09:16 +0000

    Handle notebook file modifications with temporary dirs

--- examples.py
+++ examples.py
@@ -5,24 +5,66 @@
 # ## Motivation
 # Process is moving away from being a runnable package with a single command-line entry-point to an importable package which can be scripted. This notebook is a good way of demonstrating the functionality of the package, and could provide a better way of working for modellers, who may wish to create their own scripts or notebooks for different tasks.
 # 
+# ## Setup
+# Currently the various classes and "utilities" scripts in Process have different interfaces and read and write files in differing manners and in locations that can't be easily controlled. To partially avoid the headaches associated with this, the code cell below defines a function to allow each example to be run in a temporary directory, much like a test. Input files are copied to this temporary directory and outputs contained there before the directory is removed.
+# 
+# This temporary directory function is only required for running the examples below and removing any modifications afterwards, not in regular use of Process where the outputs will want to be preserved. Further development work will unify these disparate ways of running Process into a common Pythonic form.
+
+# %%
+import process
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from shutil import copy
+
+# Define project root dir
+proj_dir = Path(process.__file__).parent.parent
+
+
+def copy_to_temp_dir(input_rel):
+    """Copy an input file to a new temp dir and return its new path.
+
+    The new TemporaryDirectory object is returned to avoid destruction of the
+    object, which results in deletion of the directory prematurely. This way
+    the cleanup() method can be used to delete the directory when required.
+    :param input_rel: file path relative to project root dir
+    :type input_rel: str
+    :return: temporary dir and absolute path to file in temp dir
+    :rtype: (TemporaryDirectory, pathlib.Path)
+    """
+    # Create temporary dir to contain the run's outputs
+    temp_dir = TemporaryDirectory()
+    temp_dir_path = Path(temp_dir.name)
+
+    # Define absolute path for input file
+    input_rel_path = Path(input_rel)
+    input_abs_path = proj_dir / input_rel_path
+
+    try:
+        assert input_abs_path.exists()
+    except AssertionError:
+        raise FileNotFoundError("Input file doesn't exist.")
+
+    # Copy input file to temp dir
+    copy(input_abs_path, temp_dir_path)
+    temp_input_path = temp_dir_path / input_abs_path.name
+
+    return temp_dir, temp_input_path
+
+
+# %% [markdown]
+# 
 # ## Basic run of Process
 # Run Process on an input file using the `SingleRun` class. This outputs an `MFILE.DAT` and an `OUT.DAT`.
 
 # %%
-import process
 from process.main import SingleRun
-from pathlib import Path
 
-# Define working directory relative to project dir and input file name
-wdir_rel = Path("tracking/baseline_2018")
-input_name = "baseline_2018_IN.DAT"
+# Define input file name relative to project dir, then copy to temp dir
+input_rel = "tracking/baseline_2018/baseline_2018_IN.DAT"
+temp_dir, temp_input_path = copy_to_temp_dir(input_rel)
 
-proj_dir = Path(process.__file__).parent.parent
-wdir_abs = proj_dir / wdir_rel
-input_path = wdir_abs / input_name
-
-# Run process on an input file
-single_run = SingleRun(str(input_path))
+# Run process on an input file in a temporary directory
+single_run = SingleRun(str(temp_input_path))
 
 
 # %% [markdown]
@@ -40,8 +82,10 @@ plot_proc.main(args=["-f", str(single_run.mfile_path)])
 summary_pdf = str(single_run.mfile_path) + "SUMMARY.pdf"
 pages = convert_from_path(summary_pdf)
 for page_no, page_image in enumerate(pages):
-    png_path = wdir_abs / f"plot_proc_{page_no + 1}.png"
+    png_path = temp_input_path.parent / f"plot_proc_{page_no + 1}.png"
     page_image.save(png_path, "PNG")
+
+# TODO Can't display the markdown pngs below due to temp_input_path being variable
 
 
 # %% [markdown]
@@ -50,27 +94,35 @@ for page_no, page_image in enumerate(pages):
 # <img src="tracking/baseline_2018/plot_proc_1.png" width="1000">
 # <img src="tracking/baseline_2018/plot_proc_2.png" width="1000">
 
+# %%
+# Delete temp dir
+temp_dir.cleanup()
+
+
 # %% [markdown]
 # ## View key output variables
 # Run the Starfire scenario using `SingleRun` to set some values on the `CostsStep` instance and then print them.
 
 # %%
-# Define working directory relative to project dir and input file name
-wdir_rel = Path("tests/regression/scenarios/starfire")
-input_name = "IN.DAT"
-
-wdir_abs_starfire = proj_dir / wdir_rel
-input_path = wdir_abs_starfire / input_name
+# Define input file name relative to project dir
+input_rel = "tests/regression/scenarios/starfire/IN.DAT"
+temp_dir, temp_input_path = copy_to_temp_dir(input_rel)
 
 # Run process on an input file
-single_run = SingleRun(str(input_path))
+single_run = SingleRun(str(temp_input_path))
 
 
 # %%
+# Print some values on the CostsStep instance
 print(
     f"Building and Site Service Infrastructure: {single_run.models.costs_step.step21:.3e} M$"
 )
 print(f"Reactor plant equipment: {single_run.models.costs_step.step22:.3e} M$")
+
+
+# %%
+# Clean up
+temp_dir.cleanup()
 
 
 # %% [markdown]
@@ -79,13 +131,28 @@ print(f"Reactor plant equipment: {single_run.models.costs_step.step22:.3e} M$")
 
 # %%
 from process.main import VaryRun
+import os
 
-wdir_rel = Path("tests/integration/data")
-conf_name = "run_process.conf"
+input_rel = "tests/integration/data/run_process.conf"
+temp_dir, temp_input_path = copy_to_temp_dir(input_rel)
 
-wdir_abs_vary = proj_dir / wdir_rel
-conf_path = wdir_abs_vary / conf_name
-vary_run = VaryRun(str(conf_path))
+# .conf file relies on a separate input file too; copy this as well
+# TODO This double input file requirement needs to be removed
+input_rel_2 = "tests/integration/data/ref_IN.DAT"
+copy(proj_dir / input_rel_2, temp_dir.name)
+
+# VaryRun uses process_config.py, which changes the current working directory
+# via os.chdir() to the temporary dir. Apart from being bad practice, once the
+# temp dir is removed, this causes Path.cwd() (as used in plot_scans.py) to
+# throw an exception when trying to return the (now deleted) CWD. Hence it
+# needs to be set back after VaryRun()
+# TODO Remove the os.chdir() from VaryRun
+cwd = Path.cwd()
+vary_run = VaryRun(str(temp_input_path))
+os.chdir(cwd)
+
+temp_dir.cleanup()
+
 
 # %% [markdown]
 # ## Plot scan
@@ -95,51 +162,21 @@ vary_run = VaryRun(str(conf_path))
 from process.io import plot_scans
 
 # Define working directory relative to project dir and input file name
-wdir_rel = Path("tests/integration/data")
-mfile_name = "scan_MFILE.DAT"
-
-wdir_abs_plot_scan = proj_dir / wdir_rel
-mfile_path = wdir_abs_plot_scan / mfile_name
+input_rel = "tests/integration/data/scan_MFILE.DAT"
+temp_dir, temp_input_path = copy_to_temp_dir(input_rel)
 
 plot_scans.main(
-    args=["-f", str(mfile_name), "-yv", "thwcndut", "--outputdir", str(wdir_abs_plot_scan)]
+    args=[
+        "-f",
+        str(temp_input_path),
+        "-yv",
+        "thwcndut",
+        "--outputdir",
+        str(temp_input_path.parent),
+    ]
 )
 
-# %% [markdown]
-# ## Clean up
-# Currently files are created in locations that can't be easily controlled; this will be changed to a temporary directory with ongoing development. For now, clean these manually here.
-
-# %%
-from os import remove
-
-# TODO This obviously needs sorting out
-# Delete SingleRun baseline2019 files
-delete = (
-    "baseline_2018_MFILE.DAT",
-    "baseline_2018_OPT.DAT",
-    "baseline_2018_OUT.DAT",
-    "baseline_2018_PLOT.DAT",
-    "baseline_2018_SIG_TF.DAT",
-    "baseline_2018_MFILE.DATSUMMARY.png",
-    "plot_proc_1.png",
-    "plot_proc_2.png",
-)
-
-for file in wdir_abs.glob("*"):
-    if file.name in delete:
-        remove(file)
-
-# Delete VaryRun files
-delete = (
-    "IN.DAT",
-    "OPT.DAT",
-    "SIG_TF.DAT",
-    "README.txt",
-)
-
-for file in wdir_abs_vary.glob("*"):
-    if file.name in delete:
-        remove(file)
+temp_dir.cleanup()
 
 
 
