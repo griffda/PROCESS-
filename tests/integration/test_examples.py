9446623610c2b4053dadcaee46b2846d7c3094d2
-l <jonathan.maddock@ukaea.uk>
2021-12-07 12:08:50 +0000

    Improve examples test documentation

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -15,11 +15,18 @@ def test_examples():
     """Run the examples.py script and check no exceptions are raised.
 
     When running a Jupyter notebook, the cwd is set to the notebook's dir. The
-    examples.py notebook relies on relative paths to files in the repository,
+    examples.ipynb notebook relies on relative paths to files in the repository,
     due to it being difficult to consistently get the location of a notebook
-    from within the notebook itself. When pytest is used to run the examples.py
-    script, the cwd is (usually) the project root dir. Hence the test needs to
-    set the cwd to the notebook's dir in order to run.
+    from within the notebook itself, so absolute paths can't be used. 
+    
+    When pytest is used to run the examples.py script (created directly from the 
+    notebook), the script uses the actual cwd instead, which is (usually) the 
+    project root dir. Therefore the examples.ipynb notebook and pytest-run 
+    examples.py script both rely on the cwd being the same, but without 
+    intervention it is different in each case.
+    
+    Hence the test needs to set the cwd to the notebook's dir before running so
+    that the examples.py script uses the same cwd as the notebook.
     """
     # Store cwd, which is usually the project root dir, then change to examples/
     cwd = Path.cwd()
