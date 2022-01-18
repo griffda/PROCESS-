6d1b65a1e209f7f1548b8c2a55af8234bdb8139b
Timothy Nunn <timothy.nunn@ukaea.uk>
2022-01-18 13:04:26 +0000

    Convert vacuum_simple to Python

--- process/vacuum.py
+++ process/vacuum.py
@@ -31,7 +31,7 @@ class Vacuum:
     def __init__(self) -> None:
         self.outfile: int = constants.nout
 
-    def run(self, output: bool):
+    def run(self, output: bool) -> None:
         """Routine to call the vacuum module
         author: P J Knight, CCFE, Culham Science Centre
 
@@ -51,9 +51,11 @@ class Vacuum:
         # MDK Check this!!
         gasld = 2.0e0 * pv.qfuel * pv.afuel * constants.umass
 
+        self.vacuum_model = f2py_compatible_to_string(vacv.vacuum_model)
+
         # vacuum_model required to be compared to a b string
         # as this is what f2py returns
-        if f2py_compatible_to_string(vacv.vacuum_model) == "old":
+        if self.vacuum_model == "old":
             pumpn, vacv.nvduct, vacv.dlscal, vacv.vacdshm, vacv.vcdimax = vac.vacuum(
                 pv.powfmw,
                 pv.rmajor,
@@ -76,11 +78,103 @@ class Vacuum:
             )
             # MDK pumpn is real: convert to integer by rounding.
             vacv.vpumpn = math.floor(pumpn + 0.5e0)
-        elif f2py_compatible_to_string(vacv.vacuum_model) == "simple":
-            vacv.niterpump = vac.vacuum_simple(int(output), self.outfile)
+        elif self.vacuum_model == "simple":
+            vacv.niterpump = self.vacuum_simple(int(output), self.outfile)
         else:
             logger.warning(f"vacuum_model seems to be invalid: {vacv.vacuum_model}")
             po.ocmmnt(
                 self.outfile,
                 f'ERROR "vacuum_model" seems to be invalid: {vacv.vacuum_model}',
             )
+
+    def vacuum_simple(self, output) -> float:
+        """Simple model of vacuum pumping system
+        author: MD Kovari, CCFE, Culham Science Centre
+
+        :param output: indicate whether output should be written to the output file, or not
+        :type output: boolean
+
+        :return npump: number of pumps for pumpdown and steady-state
+        :type npump: real
+        """
+
+        # Steady-state model (super simple)
+        # One ITER torus cryopump has a throughput of 50 Pa m3/s = 1.2155e+22 molecules/s
+        # Issue #304
+        niterpump = pv.qfuel / vacv.pumptp
+
+        # Pump-down:
+        # Pumping speed per pump m3/s
+        pumpspeed = (
+            vacv.pumpspeedmax
+            * vacv.pumpareafraction
+            * vacv.pumpspeedfactor
+            * pv.sarea
+            / tfv.n_tf
+        )
+
+        wallarea = (pv.sarea / 1084.0e0) * 2000.0e0
+        # Required pumping speed for pump-down
+        pumpdownspeed = (vacv.outgasfactor * wallarea / vacv.pbase) * tv.tdwell ** (
+            -vacv.outgasindex
+        )
+        # Number of pumps required for pump-down
+        npumpdown = pumpdownspeed / pumpspeed
+
+        # Combine the two (somewhat inconsistent) models
+        # Note that 'npump' can be constrained by constraint equation 63
+        npump = max(niterpump, npumpdown)
+
+        #  Output section
+        if output:
+
+            po.oheadr(self.outfile, "Vacuum System")
+            po.ovarst(
+                self.outfile,
+                "Switch for vacuum pumping model",
+                "(vacuum_model)",
+                '"' // self.vacuum_model // '"',
+            )
+            po.ocmmnt(
+                self.outfile,
+                "Simple steady-state model with comparison to ITER cryopumps",
+            )
+            po.ovarre(
+                self.outfile,
+                "Plasma fuelling rate (nucleus-pairs/s)",
+                "(qfuel)",
+                pv.qfuel,
+                "OP ",
+            )
+            po.ocmmnt(
+                self.outfile, "Number of high vacuum pumps, each with the throughput"
+            )
+            po.ocmmnt(
+                self.outfile,
+                " of one ITER cryopump (50 Pa m3 s-1 = 1.2e+22 molecules/s),",
+            )
+            po.ovarre(
+                self.outfile,
+                " all operating at the same time",
+                "(niterpump)",
+                niterpump,
+                "OP ",
+            )
+
+            po.ovarre(self.outfile, "Dwell time", "(tdwell)", tv.tdwell)
+            po.ovarre(
+                self.outfile,
+                "Number of pumps required for pump-down",
+                "(npumpdown)",
+                npumpdown,
+                "OP ",
+            )
+            po.ovarre(
+                self.outfile,
+                "Number of pumps required overall",
+                "(npump)",
+                npump,
+                "OP ",
+            )
+
+        return npump
