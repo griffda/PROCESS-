012940af7ddc179656835b97e8d67e2a859ff5e5
Sam Gubbins <you@example.com>
2022-06-14 11:02:30 +0100

    Last test added

--- tests/unit/test_impurity_radiation.py
+++ tests/unit/test_impurity_radiation.py
@@ -114,3 +114,26 @@ def test_Zav_of_te(imp_element_index, te, expected_zav_of_te):
     zav_of_te = impurity_radiation_module.zav_of_te(imp_element_index, te)
 
     assert pytest.approx(zav_of_te) == expected_zav_of_te
+
+
+@pytest.mark.parametrize(
+    "element_label, expected_element2index",
+    [
+        (
+            "H_",
+            1,
+        )
+    ],
+)
+def test_element2index(element_label, expected_element2index):
+    """Tests `element2index` function.
+
+    :param element_label: impurity name
+    :type element_label: string
+
+    :param expected_element2index: Returns the index of the element in the impurity array with a given name
+    :type expected_element2index: int
+    """
+    element2index = impurity_radiation_module.element2index(element_label)
+
+    assert pytest.approx(element2index) == expected_element2index
