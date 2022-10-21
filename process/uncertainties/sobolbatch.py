b2a73a5c51fd57210fff00a20a2052156fa681b2
griffda <griffiths1.thomas@gmail.com>
2022-10-21 10:43:41 +0100

    last commit

--- process/uncertainties/sobolbatch.py
+++ process/uncertainties/sobolbatch.py
@@ -1,6 +1,8 @@
 from process.uncertainties import evaluate_uncertainties
 
+
+
 #args = ["-f", "home/griff/process/process/uncertainties/config_evaluate_uncertainties.json", "-m", "run_sobol_method"]
 
-sob = evaluate_uncertainties.main(["-f", "config_evaluate_uncertainties.json", "-m", "sobol_method"])
-sob()
+#sob = evaluate_uncertainties.main(["-f", "config_evaluate_uncertainties.json", "-m", "sobol_method"])
+#sob()
