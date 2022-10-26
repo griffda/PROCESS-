c59678e1e91010192283c3269e71493a5cc6772a
griffda <griffiths1.thomas@gmail.com>
2022-10-26 15:33:30 +0100

    last commit

--- process/uncertainties/README.txt
+++
@@ -1,1 +0,0 @@
- Error status: 2.0  Error ID: 277.0
--- process/uncertainties/sobolbatch.py
+++
@@ -1,5 +0,0 @@
-from process.uncertainties import evaluate_uncertainties
-
-sob = evaluate_uncertainties.main(["-f", "/home/griff/process/process/uncertainties/config_evaluate_uncertainties.json", "-m", "morris_method"])
-sob()
-
