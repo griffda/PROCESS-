be909871832393fa7c155bcebd15f570166dc0b7
griffda <griffiths1.thomas@gmail.com>
2022-12-02 17:01:55 +0000

    latest commit since HPC clone

--- griff_work/morris/morris.py
+++ griff_work/morris/morris.py
@@ -1,5 +1,5 @@
 from process.uncertainties import evaluate_uncertainties
 
-sob = evaluate_uncertainties.main(["-f", "/home/griff/process/griff_work/morris/config_evaluate_uncertainties.json", "-m", "morris_method"])
+sob = evaluate_uncertainties.main(["-f", "/home/griff/process/griff_work/morris/config_evaluate_uncertainties_edit.json", "-m", "morris_method"])
 
 
--- griff_work/sobol/sobolbatch.py
+++ griff_work/sobol/sobolbatch.py
@@ -1,5 +1,5 @@
 from process.uncertainties import evaluate_uncertainties
 
-sob = evaluate_uncertainties.main(["-f", "/home/griff/process/griff_work/sobol/config_evaluate_uncertainties.json", "-m", "sobol_method"])
+sob = evaluate_uncertainties.main(["-f", "/home/griff/process/griff_work/sobol/config_evaluate_uncertainties_edit.json", "-m", "sobol_method"])
 
 
