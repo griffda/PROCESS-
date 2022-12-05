61aac73423f54e787b24ff1d20179e8709e32b96
griffda <griffiths1.thomas@gmail.com>
2022-12-05 16:33:41 +0000

    HPC Files

---
+++ griff_work/HPC/monte_carlo/monte_carlo_plot.py
@@ -0,0 +1,3 @@
+from process.uncertainties import hdf_to_scatter_plot
+
+plot = hdf_to_scatter_plot.main(["-i", "/home/griff/process/griff_work/monte_carlo/monte_carlo_output/uncertainties_data.h5", "-v", "fpnetel","ohcth", "cboot", "dene", "-sf", "pdf", "-p"])
